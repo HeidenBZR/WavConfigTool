772a164ee266661cdf707299df24ad1fe5fa7ec7
Heiden.BZR <heiden.bzr@gmail.com>
2019-03-10 23:11:54 +0300

    Исправить версию в предыдущей версии

--- WavConfigTool/MainWindow.Control.cs
+++ WavConfigTool/MainWindow.Control.cs
@@ -37,7 +37,7 @@ namespace WavConfigTool
         List<WavControl> WavControls;
         public static WavConfigPoint Mode = WavConfigPoint.V;
 
-        public readonly Version Version = new Version(0, 1, 5, 2);
+        public readonly Version Version = new Version(0, 1, 6, 0);
 
         public static string TempDir
         {
