6c4cd6f2fe19054c491f628db519dc42ac7ebd67
Heiden <heiden.bzr@gmail.com>
2020-02-22 02:39:47 +0300

    ver

--- WavConfigTool/ViewModels/MainWindowViewModel.cs
+++ WavConfigTool/ViewModels/MainWindowViewModel.cs
@@ -18,7 +18,7 @@ namespace WavConfigTool.ViewModels
     {
         public static readonly Version Version = new Version(0, 2, 0, 0);
 
-        public int AlphaVersion => 24;
+        public int AlphaVersion => 25;
 
         public ProjectViewModel ProjectViewModel { get; set; }
         public Project Project => ProjectManager.Project;
