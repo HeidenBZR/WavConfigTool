e04fa8138899a934fd5529773c3b4a5e8e466949
Heiden.BZR <32395982+HeidenBZR@users.noreply.github.com>
2021-02-22 00:46:33 +0300

    change spectrogram palette

--- WavConfigTool/Classes/Spectrogram.cs
+++ WavConfigTool/Classes/Spectrogram.cs
@@ -107,7 +107,7 @@ namespace WavConfigTool.Classes
             ///modify the indexed palette
             ColorPalette pallette = bitmap.Palette;
             for (int i = 0; i < 256; i++)
-                pallette.Entries[i] = System.Drawing.Color.FromArgb(255 - i, 0, 0, 255);
+                pallette.Entries[i] = System.Drawing.Color.FromArgb(255 / 2 - i/2, 255/2 - i/2, 255 - i, 255);
             bitmap.Palette = pallette;
 
             /// prepare to access data via the bitmapdata object
